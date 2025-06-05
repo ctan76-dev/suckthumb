413df1f328eda8fecb0b6c15ddb1677be3c938ec
ctan76-dev <chenwenhua76@gmail.com>
2025-06-05 22:17:36 +0000

    commit database types ts update

--- lib/database.types.ts
+++ lib/database.types.ts
@@ -1,1 +1,33 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json }
+  | Json[]
+
+export interface Database {
+  public: {
+    Tables: {
+      posts: {
+        Row: {
+          id: number
+          created_at: string
+          content: string | null
+        }
+        Insert: {
+          content?: string | null
+        }
+        Update: {
+          content?: string | null
+        }
+        Relationships: []
+      }
+    }
+    Views: {}
+    Functions: {}
+    Enums: {}
+    CompositeTypes: {}
+  }
+}
 
