b9b992b6952905f9bfb9287e26d4a0dc67cc58a5
ctan76-dev <chenwenhua76@gmail.com>
2025-06-12 16:51:16 +0000

    commit Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+// tailwind.config.js
+export default {
+  // force all utilities to be !important
+  important: true,   
+
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: { extend: {} },
+  theme: {
+    extend: {},
+  },
   plugins: [],
-};
+}
