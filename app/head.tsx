d18816b7d8e8ac6108401deb9b0be597de6fc124
ctan76-dev <chenwenhua76@gmail.com>
2025-06-21 13:08:28 +0000

    commit update

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+// app/head.tsx
+export default function Head() {
+  return (
+    <>
+      <title>SuckThumb</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </>
+  );
+}
